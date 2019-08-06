9e41455cc96a71e7bb367be546845223c49bfada
奶嘴偷走初吻 <46439201+Iceprism@users.noreply.github.com>
2019-08-07 01:40:35 +0800

    new

--- car2.ino
+++ car2.ino
@@ -17,129 +17,136 @@ int ENB = 12 ;//后轮使能 PWM！
 
 void setup() {
     //输入端
-	pinMode(D0,INPUT);
-	pinMode(D1,INPUT);
-	pinMode(D2,INPUT);
-	pinMode(D3,INPUT);
-	pinMode(VT,INPUT);
-	//输出端 驱动
-	pinMode(IN1,OUTPUT);
-	pinMode(IN2,OUTPUT);
-	pinMode(IN3,OUTPUT);
-	pinMode(IN4,OUTPUT);	
-	//使能端 因为PWM的ENB不需要定义、
-	pinMode(ENA,OUTPUT);
-	pinMode(ENB,OUTPUT);
-	//ENB
-	
+  pinMode(D0,INPUT);
+  pinMode(D1,INPUT);
+  pinMode(D2,INPUT);
+  pinMode(D3,INPUT);
+  pinMode(VT,INPUT);
+  //输出端 驱动
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);  
+  //使能端 因为PWM的ENB不需要定义、
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  //ENB
+  
 }
 
 void loop() {
-	
-	//if(digitalRead(VT) == HIGH) //无信号即停机
-	//{
-		if((digitalRead(D1) == HIGH)||(digitalRead(D2) == HIGH))
-		{
-			digitalWrite(IN3,LOW);
-			digitalWrite(IN4,LOW);
-			digitalWrite(ENB,HIGH);
-			/*for(;NOWPWM>PWMnum;NOWPWM -= 5)
-			{
-				analogWrite(ENB,NOWPWM);
-			}*/
-			if(digitalRead(D2) == HIGH)// 前进
-			{
-				digitalWrite(IN3,LOW);
-				digitalWrite(IN4,HIGH);
-				
-				
-				if(digitalRead(D3) == HIGH)// 左转
-				{
-					digitalWrite(ENA,HIGH);//启动使能端
-					digitalWrite(IN1,HIGH);
-					digitalWrite(IN2,LOW);
-				}
-				if(digitalRead(D0) == HIGH)// 右转
-				{
-					digitalWrite(ENA,HIGH);//启动使能端
-					digitalWrite(IN1,LOW);
-					digitalWrite(IN2,HIGH);
-				}
-			}
-			if(digitalRead(D1) == HIGH)//后退
-			{
-				digitalWrite(IN3,HIGH);
-				digitalWrite(IN4,LOW);
-								
-				if(digitalRead(D3) == HIGH)// 左转
-				{
-					digitalWrite(ENA,HIGH);//启动使能端
-					digitalWrite(IN1,HIGH);
-					digitalWrite(IN2,LOW);
-				}
-				if(digitalRead(D0) == HIGH)// 右转
-				{
-					digitalWrite(ENA,HIGH);//启动使能端
-					digitalWrite(IN1,LOW);
-					digitalWrite(IN2,HIGH);
-				}
-			}
-			/*else //归零 不知道有什么用
-			{
-				digitalWrite(IN3,LOW);
-				digitalWrite(IN4,LOW);
-			}*/
-			//左右判断
-			if((digitalRead(D3) == HIGH)||(digitalRead(D0) == HIGH))
-			{
-				digitalWrite(ENA,HIGH);//启动使能端
-				if(digitalRead(D3) == HIGH)// 左转
-				{
-					digitalWrite(IN1,HIGH);
-					digitalWrite(IN2,LOW);
-				}
-				if(digitalRead(D0) == HIGH)// 右转
-				{
-					digitalWrite(IN1,LOW);
-					digitalWrite(IN2,HIGH);
-				}
-				/*else
-				{
-					digitalWrite(IN1,LOW);
-					digitalWrite(IN2,LOW);
-				}*/
-			}
-			/*else 
-			{
-				digitalWrite(ENA,LOW);
-			}*/
-			
-		}
-		/*else
-		{
-			digitalWrite(ENB,LOW);
-		}*/
-	//}
-	/*
-	else
-	{
-		digitalWrite(IN1,LOW);
-		digitalWrite(IN2,LOW);
-		digitalWrite(IN3,LOW);
-		digitalWrite(IN4,LOW);
-		
-		digitalWrite(ENA,LOW);
-		digitalWrite(ENB,LOW);
-	}
-	*/
-	if(digitalRead(VT) == LOW)
-	{
-		digitalWrite(IN1,LOW);
-		digitalWrite(IN2,LOW);
-		digitalWrite(IN3,LOW);
-		digitalWrite(IN4,LOW);
-	}
-	
+  
+  //if(digitalRead(VT) == HIGH) //无信号即停机
+  //{
+    if((digitalRead(D1) == HIGH)||(digitalRead(D2) == HIGH))
+    {
+      digitalWrite(IN3,LOW);
+      digitalWrite(IN4,LOW);
+      digitalWrite(ENB,HIGH);
+
+      analogWrite(ENB,110);
+
+      if(digitalRead(D2) == HIGH)// 前进
+      {
+        digitalWrite(IN3,LOW);
+        digitalWrite(IN4,HIGH);
+        analogWrite(ENB,60);
+        
+        if(digitalRead(D3) == HIGH)// 左转
+        {
+          digitalWrite(ENA,HIGH);//启动使能端
+          digitalWrite(IN1,HIGH);
+          digitalWrite(IN2,LOW);
+          analogWrite(ENB,60);
+        }
+        if(digitalRead(D0) == HIGH)// 右转
+        {
+          digitalWrite(ENA,HIGH);//启动使能端
+          digitalWrite(IN1,LOW);
+          digitalWrite(IN2,HIGH);
+          analogWrite(ENB,60);
+        }
+      }
+      if(digitalRead(D1) == HIGH)//后退
+      {
+        digitalWrite(IN3,HIGH);
+        digitalWrite(IN4,LOW);
+        analogWrite(ENB,60);
+                
+        if(digitalRead(D3) == HIGH)// 左转
+        {
+          digitalWrite(ENA,HIGH);//启动使能端
+          digitalWrite(IN1,HIGH);
+          digitalWrite(IN2,LOW);
+          analogWrite(ENB,60);
+        }
+        if(digitalRead(D0) == HIGH)// 右转
+        {
+          digitalWrite(ENA,HIGH);//启动使能端
+          digitalWrite(IN1,LOW);
+          digitalWrite(IN2,HIGH);
+          analogWrite(ENB,60);
+        }
+      }
+      /*else //归零 不知道有什么用
+      {
+        digitalWrite(IN3,LOW);
+        digitalWrite(IN4,LOW);
+      }*/
+     /*
+      //左右判断
+      if((digitalRead(D3) == HIGH)||(digitalRead(D0) == HIGH))
+      {
+        digitalWrite(ENA,HIGH);//启动使能端
+        if(digitalRead(D3) == HIGH)// 左转
+        {
+          digitalWrite(IN1,HIGH);
+          digitalWrite(IN2,LOW);
+        }
+        if(digitalRead(D0) == HIGH)// 右转
+        {
+          digitalWrite(IN1,LOW);
+          digitalWrite(IN2,HIGH);
+        }
+        /*else
+        {
+          digitalWrite(IN1,LOW);
+          digitalWrite(IN2,LOW);
+        }*/
+      //}
+	  
+      /*else 
+      {
+        digitalWrite(ENA,LOW);
+      }*/
+      
+    }
+    /*else
+    {
+      digitalWrite(ENB,LOW);
+    }*/
+  //}
+  /*
+  else
+  {
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,LOW);
+    
+    digitalWrite(ENA,LOW);
+    digitalWrite(ENB,LOW);
+  }
+  */
+  if(digitalRead(VT) == LOW)
+  {
+    digitalWrite(IN1,LOW);
+    digitalWrite(IN2,LOW);
+    digitalWrite(IN3,LOW);
+    digitalWrite(IN4,LOW);
+    NOWPWM = 110 ;
+  }
+  
 
   
   
